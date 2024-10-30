489c32b0f32dc72d0f733521ca0485968ccddaa9
Fermin2049 <fermin2049@gmail.com>
2024-10-30 20:05:50 -0300

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation ("com.google.android.material:material:1.4.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
